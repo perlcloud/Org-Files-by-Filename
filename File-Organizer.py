b2405362063f2bb7973c3a37d900c7b0fececba5
perlcloud <cloud.perl@gmail.com>
2018-12-19 22:05:40 -0500

    Improved posn_helper

--- File-Organizer.py
+++ File-Organizer.py
@@ -1,16 +1,17 @@
 import os
 import sys
 
-def posn_help():
-    # Returns 1st file split with index positions
+def posn_helper():
+   # Returns 1st file split with index positions
     for file in file_list[:1]:
         file_pieces = file.split(delim)
-        p = 0
-        print('\n--------------------------')
+        line_length = len(max(file_pieces, key = len)) + 10
+        index = 0
+        print('\n' + ('-' * line_length))
         for piece in file_pieces:
-            print('Index ' + str(p) + ' = ' + file_pieces[p])
-            p += 1
-        print('--------------------------\n')
+            print('Index ' + str(index) + ' = ' + file_pieces[index])
+            index += 1
+        print(('-' * line_length) + '\n')
 
 
 def progress_bar(progress):
@@ -38,7 +39,7 @@ def clean_list(path):
 src_dir = os.path.dirname(os.path.realpath(__file__))
 file_list = clean_list(src_dir)
 delim = input('Enter a delimiter character: ')
-posn_help() # Displays list of possible posn choices
+posn_helper() # Displays list of possible posn choices
 posn = int(input('Enter index number for folder name: '))
 print('')
 file_count = 0
