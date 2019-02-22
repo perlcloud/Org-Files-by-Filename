53e866606dc6495bdfb8615cabbdaa441213d04c
perlcloud <cloud.perl@gmail.com>
2019-02-22 00:57:25 -0500

    README update and split file extention

--- File-Organizer.py
+++ File-Organizer.py
@@ -1,64 +1,103 @@
 import os
 import sys
 
-def posn_helper():
-   # Returns 1st file split with index positions
-    for file in file_list[:1]:
-        file_pieces = file.split(delim)
-        line_length = len(max(file_pieces, key = len)) + 10
+
+def file_splitter(filename):
+    """Split a filename into pieces"""
+    filename_pieces = filename.split(delimiter)
+
+    # Remove the last file piece and split file extension
+    new_values = filename_pieces[-1].split('.')
+    filename_pieces.pop(-1)
+    for value in new_values:
+        filename_pieces.append(value)
+
+    return filename_pieces
+
+
+def position_helper():
+    """Returns 1st file in the dir split with index positions"""
+    for file_name in file_list[:1]:
+        file_bits = file_splitter(file_name)
+        line_length = len(max(file_bits, key=len)) + 13
         index = 0
         print('\n' + ('-' * line_length))
-        for piece in file_pieces:
-            print('Index ' + str(index) + ' = ' + file_pieces[index])
+        for x in file_bits:
+            print('Index ', str(index), ' = ', file_bits[index])
             index += 1
         print(('-' * line_length) + '\n')
 
 
 def progress_bar(progress):
-    barLength = 50
-    block = int(round(barLength*progress))
-    text = '\rProgress: [{0}] {1}'.format('#'*block + '-'*(barLength-block), progress*100)
-    sys.stdout.write(text)
+    """Displays a progress bar"""
+    bar_length = 50
+    block = int(round(bar_length * progress))
+    text = 'Progress: [{0}] {1}'.format('#' * block + '-' * (bar_length - block),
+                                        progress * 100)
+    # Print progress after removing the previous progress
+    sys.stdout.write('\r' + text)
     sys.stdout.flush()
 
 
 def clean_list(path):
-    # Remove unwanted files from source folder
-    file_list = [f for f in os.listdir(path)  # Removes directories 
-                if os.path.isfile(os.path.join(path, f))]
+    """Ignore unwanted files in source folder"""
+    # Remove directories 
+    clean_file_list = [f for f in os.listdir(path)
+                       if os.path.isfile(os.path.join(path, f))]
+
+    # List files to ignore
     bad_files = ['desktop.ini',
-                os.path.basename(__file__)]
-                # TODO: Ignore hidden files & self when compiled 
-    for junk in bad_files:
-        if junk in file_list:
-            file_list.remove(junk)
-    return file_list
+                 os.path.basename(__file__)]
+    # TODO: Ignore hidden files & self when compiled
+
+    # Loop through bad files and remove from list
+    for found_file in bad_files:
+        if found_file in clean_file_list:
+            clean_file_list.remove(found_file)
+    return clean_file_list
 
 
-# Set variables
-src_dir = os.path.dirname(os.path.realpath(__file__))
+# Get path from user if no path entered
+src_dir = r'M:\Dropbox\Sandbox\Org-Files-by-Filename\sample-files'
+if not src_dir:
+    src_dir = input('Enter the path to your directory: ')
+
 file_list = clean_list(src_dir)
-delim = input('Enter a delimiter character: ')
-posn_helper() # Displays list of possible posn choices
-posn = int(input('Enter index number for folder name: '))
-print('')
+
+# Get user input
+delimiter = input('Enter a delimiter character: ')
+
+# Displays list of possible position choices
+position_helper()
+
+# Get user input
+position = int(input('Enter index number for folder name: '))
+
 file_count = 0
 dir_count = 0
+err_count = 0
 
 # Loop through each file and send to folder
 for file in file_list:
-    file_pieces = file.split(delim)
-    dst_dir = os.path.join(src_dir, file_pieces[posn])
+    file_pieces = file_splitter(file)
+    dst_dir = os.path.join(src_dir, file_pieces[position])
+
     # Create new folder if it does not already exist
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
         dir_count += 1
-    os.rename(os.path.join(src_dir, file), os.path.join(dst_dir, file))
-    file_count += 1
-    progress_bar((file_count)/len(file_list))
+
+    # Move file into new folder
+    try:
+        os.rename(os.path.join(src_dir, file), os.path.join(dst_dir, file))
+        file_count += 1
+        progress_bar(file_count / len(file_list))
+    except:
+        err_count += 1
 
 # Print results
-print('\nNew Directories Created: ' + str(dir_count))
-print('Files Moved:             ' + str(file_count))
+print('\nNew Directories Created: ', str(dir_count))
+print('Files Moved:             ', str(file_count))
+print('Failed Files:            ', str(err_count))
 
 input('Press enter to exit')
--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-## Organize Files By Filename
-Organize files into folders by extracting part of the filename to be used as the folder name.
+# Organize Files By Filename
+###### Organize files into folders by extracting part of the filename to be used as the folder name.
 
-## Example
-Say you have many files with the following naming convention:
-Project-Name_Product-ID_unique-id.ext
-By telling the program to split the file name on _ and that you want the 2nd part
-as your folder name, all files will be organized into folders by Product-ID.
+## Use
+#### Path
+Set the value of the `src_dir` variable to your path, or leave blank to be prompted for the path.
+#### Delimiter
+Filename example: `Project-Name_Product-ID_unique-id.ext`
 
-This is intended to be easy to use, but is not yet a finished product...
+Input your delimiter when prompted (eg `_`) to tell the script what to split the file name on.
+#### Index
+After specifying your delimiter, you will be presented with a sample file split with an index number for each bit:
+```
+Index  0  =  Project-Name
+Index  1  =  Product-ID
+Index  2  =  unique-id
+Index  3  =  ext
+```
+Enter the index number corresponding to the part you want to be used as the folder name.
+
+_Note: The end of the file is always split on `.` to extract the filename. This is potentially problematic if a filename has more than one `.` in it._
+#### Conclusion
+When done, you will be presented with a log of what work was done. In the event of an error, the file is skipped.
+```
+New Directories Created:  5
+Files Moved:              84
+Failed Files:             2
+```
